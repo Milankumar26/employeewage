b63c182056e5756ae8b2f62e3c06321ed358f361
Milankumar26 <milanramchandani26@gmail.com>
2023-01-27 09:31:08 +0530

    added attendance feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args)
+    {
+        int isPresent = 0;
+        int isAbsent = 1;
+        int checkAttendance = (int)((Math.random()*10)%2);
+        if (checkAttendance==isPresent){
+            System.out.println("Employee is present");
+        }
+        else {
+            System.out.println("Employee is absent");
+        }
     }
 }
