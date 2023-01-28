90f6b0bfb0277d45d0103244c742f28e45a4947f
Milankumar26 <milanramchandani26@gmail.com>
2023-01-28 09:03:06 +0530

    added parttime feature

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,20 @@ public class Main {
     public static void main(String[] args)
     {
         int isPresent = 0;
+        int isPartTime=2;
         int isAbsent = 1;
         int dailySalary;
         int workHours;
         int salaryPerhour = 20;
-        int checkAttendance = (int)((Math.random()*10)%2);
+        int checkAttendance = (int)((Math.random()*10)%3);
         if (checkAttendance==isPresent){
             System.out.println("Employee is present");
             workHours=8;
+        }
+        else if (checkAttendance==isPartTime)
+        {
+            System.out.println("Employee is part time");
+            workHours=4;
         }
         else {
             System.out.println("Employee is absent");
